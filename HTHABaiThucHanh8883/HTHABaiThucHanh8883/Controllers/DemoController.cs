c05153503ef920b5dd389a825b3a43ab9009d6fe
haianh1999 <hoanghaianh150699@gmail.com>
2022-03-01 08:39:32 +0700

    -aCommit demo

---
+++ HTHABaiThucHanh8883/HTHABaiThucHanh8883/Controllers/DemoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTHABaiThucHanh8883.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Demo1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Demo2(string MaSinhVien, string HoTenSinhVien)
+        {
+            ViewBag.Thongtin = MaSinhVien + "" + HoTenSinhVien;
+            return View();
+        }
+    }
+}
